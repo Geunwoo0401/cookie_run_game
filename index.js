5ce7a447e5b3091e809285e61762c4adc511fdd3
Geunwoo0401 <115263008+Geunwoo0401@users.noreply.github.com>
2022-10-29 16:28:36 +0900

    점프수정
    점프 두번 수정

--- index.js
+++ index.js
@@ -44,6 +44,15 @@ var Spider여러개 = [];
 var jumptimer = 0;
 var animation;
 
+// 점프 최대 횟수
+var maximum_jump_count = 2;
+
+// 현재 점프 횟수
+var jump_count = 0;
+
+// 땅의 y좌표(점프 횟수 초기화 비교할떄)
+var ground_y_coordination = 200;
+
 
 function 프레임마다실행할거(){
     animation = requestAnimationFrame(프레임마다실행할거); // 1초에 60번 실행하면 애니메이션 만들수있음
@@ -51,7 +60,7 @@ function 프레임마다실행할거(){
 
     ctx.clearRect(0,0, canvas.width, canvas.height); // 캔버스 지우는방법
 
-    if(timer % 250 === 0){ // 1초에 (120)프레임 설정  장애물 스폰 시간
+    if(timer % 500 === 0){ // 1초에 (120)프레임 설정  장애물 스폰 시간
         var spider = new Spider(); // 장애물 스폰
         Spider여러개.push(spider);// 장애물 여러개 스폰
     }
@@ -65,9 +74,8 @@ function 프레임마다실행할거(){
         Collision(ghost, a);
         a.draw();
     });
-    
-    // 점프기능
 
+    // 점프기능
     if( jump == true ){
         ghost.y--; // 100 프레임 지나면 점프 중단
         jumptimer++; // 점프시 프레임마다 +1
@@ -82,8 +90,9 @@ function 프레임마다실행할거(){
         jumptimer = 0;
     }
     // spider.draw();
-    // ghost.y -= 2;  점프 속도
+    // ghost.y -= 2;  // 점프 속도
     ghost.draw();
+    IsPlayerOnGround();
 }
 프레임마다실행할거();
 
@@ -97,11 +106,24 @@ function Collision(ghost, Spider){
             alert("GAME OVER");
     }
 }
+// 땅에 닿으면 점프 초기화
+function IsPlayerOnGround(){
+  if (ghost.y == ground_y_coordination){
+    jump_count = 0;
+  }
+}
 
 
 var jump = false;
 document.addEventListener('keydown', function (e){
     if (e.code === 'Space'){
+      if (maximum_jump_count > jump_count){
         jump = true;
+        jump_count++;
+      }
+      else {
+        console.log("점프 이미 두번함");
+      }
+
     }
 });
